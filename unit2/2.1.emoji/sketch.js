f199eb7b2c44eaaf31ec8b1190b8b4eb0956dd22
Yosays <yosyasmarta@gmail.com>
2024-09-23 11:27:46 -0400

    asdfasdf
    asdfsadf

--- unit2/2.1.emoji/sketch.js
+++ unit2/2.1.emoji/sketch.js
@@ -10,5 +10,25 @@ function draw() {
   background(220);
 
   rectMode(CENTER);
- 
+  //Headtop
+  stroke('gold');
+  fill('gold');
+  circle(200,200,250);
+  //Eyes.1
+  stroke('white');
+  fill('white');
+  circle(155,155,60);
+  //Eyes.2
+  stroke('white');
+  fill('white');
+  circle(245,155,60);
+  //Pupils.1
+  stroke('black');
+  fill('black');
+  circle(240,155,20);
+  //Pupils.2
+  stroke('black');
+  fill('black');
+  circle(160,155,20);
+  
 }
