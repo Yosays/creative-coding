4904f666d6b0f0eccbc29a8b70a0b1116740761f
Yosays <yosyasmarta@gmail.com>
2024-10-01 10:47:17 -0400

    asdf
    asdf

--- unit2/2.1.emoji/sketch.js
+++ unit2/2.1.emoji/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   // create a canvas
-  createCanvas(400, 400);
+  createCanvas(800, 800);
   //https://emojipedia.org/microsoft/windows-11-23h2-june-2024-update/face-without-mouth <---- this is the emoji I attempted to print out
   // disable animation
   noLoop();
@@ -9,6 +9,7 @@ function setup() {
 function draw() {
   background(220);
 
+  //No Reaction Face
   rectMode(CENTER);
   //Headtop
   stroke('gold');
@@ -30,5 +31,25 @@ function draw() {
   stroke('black');
   fill('black');
   circle(160,155,20);
+
+  //Throwing Up
+  stroke(96,245,66);
+  fill(96,245,66);
+  circle(400,400,250);
+  //Eyes.1
+  stroke('white');
+  fill('white');
+  circle(440,385,62.5);
+  //Eyes.2
+  stroke('whtie');
+  fill('white');
+  circle(360,385,62,5);
+  //Eyebrow.1
+  stroke('black');
+  strokeWeight(10);
+  curve(700,505,500,370,465,375,500,400);
   
+
+  
+
 }
--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -8,4 +8,25 @@ function setup() {
 
 function draw() {
   background(220);
+// moves the grid to the right 100 pixels ahead of the loop 
+translate(100,100);
+
+  for (let x = 0; x < 8; x++){
+    for (let y = 0; y < 8; y++){
+      
+      push();
+      //
+      translate(x * 100, y * 100);
+      // I wanted to emphasize the stroke weight due to base being to small
+      // 
+      strokeWeight(random(10));
+      // made 3 random varaibles each to compltet the value of R-G-B 
+      stroke(random(100,255),random(50,255),random(0,255));
+      // made the fill this color to give a better contrast 
+      fill(190, 176, 165);
+      ellipse(0,0,100,100);
+      pop();
+
+  }
+}
 }
