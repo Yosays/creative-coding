1b7ed9981880b4de195fd01c210f9fdfe44e093f
Yosays <yosyasmarta@gmail.com>
2024-10-17 00:25:58 -0400

    Update sketch.js

--- unit2/landscape/sketch.js
+++ unit2/landscape/sketch.js
@@ -7,11 +7,12 @@ function setup() {
 
   // create the canvas
   createCanvas(800, 800);
-  noLoop();
+  //noLoop();
+  
 }
-
+background(220);
 function draw() {
-  background(220);
+  
   
   
  
@@ -19,14 +20,17 @@ function draw() {
 
 
 
-  for (let x = 0; x < 1; x++){
-
-    for (let y = 0; x < 1; y++){
+  for (let x = 0; x < 100; x++){
+    for (let y = 0; y < 100; y++){
+    
       let n = noise(x * noiseScale, y * noiseScale) * 255;
       //noise(x,y);
-      stroke(n, 0, 255 - n);
-      strokeWeight(10);
+      //translate(x * 100, y *100);
+      stroke('purple');
+      strokeWeight(1);
+      
       point(x * 10, y * 10);
+      
       
   // render the image
   //image(img,10,10,100,100);
