0c2320d0311eb4ba6a6c8b9654a42c69449c7524
Yosays <yosyasmarta@gmail.com>
2024-11-20 21:38:08 -0500

    asdf
    asdf

--- unit3/3.3.ripples/sketch.js
+++ unit3/3.3.ripples/sketch.js
@@ -1,18 +1,50 @@
-let x = 0;
-let y = 0;
-let d = 0;
+let ripple_x = 0;
+let ripple_y = 0;
+let ripple_d = 0;
+
+
+let ripplers = [];
+
+
+class Rippler {
+  constructor (x,y){
+    this.x = x,
+    this.y = y,
+    this.d = 0
+    
+  }
+  draw(){
+    this.d += 1;
+    stroke(51, 123, 212);
+    strokeWeight(1);
+    noFill();
+    circle (this.x, this.y, this.d);
+  }
+}
+
+
 
 function setup() {
   createCanvas(800, 800);
 }
 
+
 function draw() {
-  background(220);
+background(52, 134, 235);
 
-  // distance formula
-d = sqrt((x - mouseX) **2 + (y - mouseY) **2);
 
-//debug
-textSize(30);
-text("d: " + d, 50, 150);
+for (let i = 0; i < ripplers.length; i++){
+  ripplers[i].draw();
 }
+
+}
+
+function mousePressed(){
+
+    ripple_x = mouseX;
+    ripple_y = mouseY;
+    ripple_d = 0;
+
+  ripplers.push(new Rippler(mouseX, mouseY));  
+}
+
