f46d9d7f17ada8b223b9ab1ea2cc9687e5d65a26
Yosays <yosyasmarta@gmail.com>
2024-12-03 11:37:58 -0500

    asdf
    asdf

--- unit3/time/sketch.js
+++ unit3/time/sketch.js
@@ -10,25 +10,25 @@ function draw() {
   background(220);
 
   // Calculate line heights based on current time
-  secondLineHeight = map(second(), 0, 59, 0, 300); // Scale seconds to 0–300 pixels
-  minuteLineHeight = map(minute(), 0, 59, 0, 300); // Scale minutes to 0–300 pixels
-  hourLineHeight = map(hour(), 0, 23, 0, 300); // Scale hours to 0–300 pixels
+  secondLineHeight = map(second(), 0, 59, 0, 300); 
+  minuteLineHeight = map(minute(), 0, 59, 0, 300); 
+  hourLineHeight = map(hour(), 0, 23, 0, 300); 
 
   // Draw the second line
   strokeWeight(4);
-  stroke(255, 0, 0); // Red for seconds
+  stroke("orange"); // Red for seconds
   let secondY = 650 - secondLineHeight;
   line(100, 650, 100, secondY);
   drawFlower(100, secondY, 25);
 
   // Draw the minute line
-  stroke(0, 255, 0); // Green for minutes
+  stroke("gold"); // Green for minutes
   let minuteY = 650 - minuteLineHeight;
   line(300, 650, 300, minuteY);
   drawFlower(300, minuteY, 25);
 
   // Draw the hour line
-  stroke(0, 0, 255); // Blue for hours
+  stroke('yellow'); // Blue for hours
   let hourY = 650 - hourLineHeight;
   line(500, 650, 500, hourY);
   drawFlower(500, hourY, 25);
