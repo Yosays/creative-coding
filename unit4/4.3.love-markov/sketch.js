4c14b1d83a917753fd13ac6765586d3811a78878
Yosays <yosyasmarta@gmail.com>
2024-12-05 15:45:54 -0500

    adsf
    asdf

--- unit4/4.3.love-markov/sketch.js
+++ unit4/4.3.love-markov/sketch.js
@@ -1,14 +1,28 @@
-
-
-let rm = RiTa.markov(2);
-
+let data = document.getElementById("source").innerText; 
+let rm = RiTa.markov(2); 
+let letter = select("#letter"); 
 
 
 function preload() {
-  result = loadStrings('');
+  
 }
 
-function setup(){
+function setup() {
+  l
+  rm.addText(data);
 
-	
+
+  let lines = rm.generate(6); 
+
+
+  letter.child(createP('My Dearest Harrington,'));
+
+
+  for (let l = 0; l < lines.length; l++) {
+    let paragraph = createP(lines[l]);
+    letter.child(paragraph);
+  }
+
+
+  letter.child(createP('Sincerely,<br>Markov'));
 }
