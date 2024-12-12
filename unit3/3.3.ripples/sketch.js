7a615d7f05ecdc7e34e9c44455e2f12f6fc2ccad
Yosays <yosyasmarta@gmail.com>
2024-12-11 21:10:01 -0500

    Update sketch.js

--- unit3/3.3.ripples/sketch.js
+++ unit3/3.3.ripples/sketch.js
@@ -1,42 +1,38 @@
-// Array to store all the rippler objects
+
 let ripplers = [];
 
-// The Rippler class represents an individual ripple effect
 class Rippler {
   constructor(x, y) {
-    // Initialize the position (x, y) and diameter (d) of the ripple
     this.x = x;
     this.y = y;
-    this.d = 0; // Initial diameter is 0 (circle will start small)
+    this.d = 0; // assign d as zero to make the ripple start small than expand
   }
 
   // Method to draw the ripple effect
   draw() {
     this.d += 1; // Increase the diameter of the ripple on each frame
-    stroke(51, 123, 212); // Set the stroke color (blue)
-    strokeWeight(1); // Set the thickness of the stroke (outline of the circle)
-    noFill(); // Don't fill the circle, just outline it
+    stroke(51, 123, 212); // Set the color blue
+    strokeWeight(1); 
+    noFill(); 
     circle(this.x, this.y, this.d); // Draw the circle with the current diameter
   }
 }
 
-// Setup function, runs once at the beginning to initialize the canvas
+
 function setup() {
-  createCanvas(800, 800); // Create an 800x800 canvas
+  createCanvas(800, 800); 
 }
 
-// Draw function, runs continuously to update the canvas
 function draw() {
-  background(52, 134, 235); // Set the background color (light blue)
+  background(52, 134, 235); // Make background light blue for a pond look
 
   // Loop through each rippler in the ripplers array and call its draw method
   for (let i = 0; i < ripplers.length; i++) {
-    ripplers[i].draw(); // Draw the ripple for each rippler object
+    ripplers[i].draw(); 
   }
 }
 
-// MousePressed function, called when the user clicks the mouse
+// Use the MousePressed function to capture where the user clicks and call for a rippler to be drawn
 function mousePressed() {
-  // Add a new rippler at the location of the mouse click (mouseX, mouseY)
   ripplers.push(new Rippler(mouseX, mouseY)); 
 }
